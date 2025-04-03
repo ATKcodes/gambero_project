1ed9ca9d8482370c45f941e6fac42d8d2140f0c8
ATKcodes <andrea@LAPTOP-110UQAD7>
2025-04-03 06:26:41 +0200

    started working on mobile app

--- job-consultation-app/capacitor.config.ts
+++ job-consultation-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.jobconsultation.app',
   appName: 'Job Consultation',
-  webDir: 'dist/job-consultation-app'
+  webDir: 'dist/job-consultation-app/browser'
 };
 
 export default config;
